8ea1710127d49815793044576bf8711667e6be11
Boyko <bmlalov@abv.bg>
2015-12-15 13:54:41 +0200

    Refactoring.

--- VatCalculator/src/com/boyko/vat/exceptions/CountryNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.boyko.vat.exceptions;
-
-public class CountryNotFoundException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public CountryNotFoundException(String msg) {
-		super(msg);
-	}
-}
