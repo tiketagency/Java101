513ca532ea0c250fd044284342f89c8723726e6a
Boyko <bmlalov@abv.bg>
2016-01-15 00:11:23 +0200

    Rafactor mp3 organizer.

--- week6/src/monday/mp3organizer/Mp3Organizer.java
+++ week6/src/monday/mp3organizer/Mp3Organizer.java
@@ -35,9 +35,11 @@ public class Mp3Organizer {
 		} else {
 			if (!destination.toFile().exists()) {
 				FileManager.createDir(destination, file, metaData);
-			}
+			}else {
+				
 			FileManager.copyFileToDir(destination, file,
 					StandardCopyOption.REPLACE_EXISTING);
+			}
 
 		}
 		deleteIfExist(destination, file);
